a95e3bc28390122ddf80058e3369577add84859c
Vincent <1032006226@qq.com>
2018-01-19 00:35:58 +0800

    心电图数据

--- app/src/main/java/com/example/vincent/customview/MyAllDateActivity.java
+++ app/src/main/java/com/example/vincent/customview/MyAllDateActivity.java
@@ -3,6 +3,13 @@ package com.example.vincent.customview;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.vincent.customview.utils.ReadAssetsFileUtils;
+import com.example.vincent.customview.view.MyDataAll;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Vincent on 2018/1/18.
@@ -10,11 +17,29 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MyAllDateActivity extends AppCompatActivity {
 
-
+    private static final String TAG = MyAllDateActivity.class.getSimpleName();
+    //数据源
+    private List<Integer> allDatas = new ArrayList<>();
+    private MyDataAll dataAll;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_all);
+        changeData(ReadAssetsFileUtils.readAssetsTxt(this,"StarCareData"));
+        dataAll = findViewById(R.id.myDataAll);
+        dataAll.addAllData(allDatas);
+    }
+
+    private void initData() {
+
+    }
+    private void changeData(String starCareData) {
+        String[] strDatas = starCareData.split("\n");
+        for (int i = 0;i<strDatas.length;i++){
+            allDatas.add(Integer.valueOf(strDatas[i].replace("\r","")));
+        }
+        Log.d(TAG, "changeData: allDatas size is "+String.valueOf(allDatas.size()));
+//        myData.addAllData(allDatas);
     }
 }
